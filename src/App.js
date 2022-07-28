bfd5c9f4e52e35c209ad20d9e6f5e20a0033c023
codessi <johansuh@gmail.com>
2022-07-28 13:33:42 -0700

    fulfilled

--- src/App.js
+++ src/App.js
@@ -6,39 +6,40 @@ import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
 
 
 function App() {
 
-//inside of Router, 
-  // make folder components
-  // Navbar.jsx
-  // import {useNavigate, useLocation}
-  // import {ReactComponent as OfferIcon} from .. localOfferIcon.svg
-  // copy 2 more Explore
-  // and PersonOutline
-  // jsx footer.navbar wrap
-  // nav.navbarNav wrap
-  // ul.navbarListItems wrap
-  // li.navbarListItem wrap
-  // <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
-  // sibling to Expl. , p content Explore
-  // copy twice
-  // 2nd  Offer
-  // 3rd  PersonOutline p Profile
-  // gt app.js
-  // import navbar
-  // insert bottom before closing Router.
-  //  check
-  // initalize useNavigate() as navigate
-  // useLocation same as above
-  // li 1st onClick  function envoke navigate passing '/'
-  // Offer
-  // Profile
-  // create const pathMatchRoute  pass route  if route equal to location.pathname  return true
-  // change fill pathMatchRoute('/')?  #2c2c2c : #8f8f8f
-  // p add class {pathMatchRoute('/')? 'navbarListItemNameActive': 'navbarListItemName'}
-  // do the rest 
+//inside of Router,  [x]
+  // make folder components []
+  // Navbar.jsx []
+  // import {useNavigate, useLocation} []
+  // import {ReactComponent as OfferIcon} from .. localOfferIcon.svg [x]
+  // copy 2 more Explore [x]
+  // and PersonOutline [x]
+  // jsx footer.navbar wrap []
+  // nav.navbarNav wrap []
+  // ul.navbarListItems wrap []
+  // li.navbarListItem wrap []
+  // <ExploreIcon fill='#2c2c2c' width='36px' height='36px' /> []
+  // sibling to Expl. , p content Explore []
+  // copy twice []
+  // 2nd  Offer []
+  // 3rd  PersonOutline p Profile []
+  // gt app.js [x]
+  // import navbar [x]
+  // insert bottom before closing Router. [x]
+  //  check [x]
+  // initalize useNavigate() as navigate [x]
+  // useLocation same as above [x]
+  // li 1st onClick  function envoke navigate passing '/' [x]
+  // Offer [x]
+  // Profile [x]
+  // create const pathMatchRoute  pass route  if route equal to location.pathname  return true [x]
+  // change fill pathMatchRoute('/')?  #2c2c2c : #8f8f8f []
+  // p add class {pathMatchRoute('/')? 'navbarListItemNameActive': 'navbarListItemName'} []
+  // do the rest  []
 
   return (
     <>
@@ -51,6 +52,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-passwords" element={<ForgotPasswords /> } />
         </Routes>
+        <Navbar /> 
       </Router>
     </>
   );
