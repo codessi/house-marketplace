acaa3afdee927c7dbb2eafef752e4746e4fb39d1
codessi <johansuh@gmail.com>
2022-07-27 19:24:03 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+// create firebase.config.js
+  //copy and paste from web
+  //import getFireStore from 'firebase/firestore'
+  // export const db = getFirestore()
+  // npm i firebase
+  // bring assets folder inside of src
+  // bring in index.css to src
+  // favicon to public
+  // delete app.css & etc
+  // gt app
+ // <> h1 My APP
+  // check
+  // gt firebase web
+  // continue
+  
+
+
+
   return (
     <div className="App">
       <header className="App-header">
