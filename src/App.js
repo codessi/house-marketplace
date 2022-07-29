ea1fd070310b55f3e422e77b8b27a8c3b51dcafc
codessi <johansuh@gmail.com>
2022-07-29 06:55:05 -0700

    register user, fulfilled

--- src/App.js
+++ src/App.js
@@ -10,31 +10,22 @@ import Navbar from "./components/Navbar";
 
 
 function App() {
-// gt signin  [x]
-  //import usestate, Link,useNavigate, ArrowRightIcon,  [x]
-  // import visibilityIcon  svg  [x]
-  // const [showPassword, set.. ] state false  [x]
-  // const formData, set... state passing object key email "" password: ""  [x]
-  // destructure formData with email and password  [x]
-  //add jsx .pageContainer  [x]
-  // header wrap [x]
-  // p.pageHeader content Welcome Back!  [x]
-  //sibling to header, main tag wrap form tag wrap input.emailInput type email   [x]placeholder email id email value {email} onChange onChnage function
-  // make onChange function  [x]
-  // jsx sibling to input, .passwordInputDiv wrap  [x]
-  // input type {showPassword? 'text' :"password"} .passwordInput placeholder   [x]Password id password value password onchange onChange
-  // sibling img.showPassword src {visibilityIcon} alt show password  onClick   []call the setShow  previousState !previousState
-  // parent of img, sibling , <Link to = '/forgot-password' .forgotPasswordLink   [x]content Forgot Password
-  // sibiling, .signInBar wrap p.signInText content Sign In  [x]
-  // sibling to p, button.signInButton wrap  [x]
-  // ArrowRightIcon fill #ffffff width 34px height 34px  [x]
-  // sibling to form, Link to /sign-up  .registerLink  wrap  [x]
-  // Sign Up Instead  [x]
-  // check  [x]
-  // onChange pass e  setFormData pass function pass previousState return   [x]object  ...previousState, key [e.target.id] value e.target.value
-  // check dev tool  [x]
 
-  
+// gotod firebase documentaton about auth [x]
+// gt sign up [x]
+  // import {getAuth, createUserWithEmailAndPassWord, updateProfile} from 'firebase/auth' []
+  // imort { db } from firbase.config [x]
+  // add onSubmit inside of form tag [x]
+  // preventdefault [x]
+  // create try catch(error) [x]
+  // create async onSubmit function [x]
+  // try,  assign auth = getAuth [x]
+  //  const userCredential = await createUser... (auth, email, password) [x]
+  //const user = userCredential.user [x]
+  // updateProfile(auth.currentUser, {displayName: name}) []
+  // navigate('/') []
+  // catch (error){  const errorCode = error.code; []
+    // const errorMessage = error.message;} []
 
   return (
     <>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "./../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "./../assets/svg/visibilityIcon.svg";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "./../firebase.config";
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-export default SignUp
+  const navigate = useNavigate();
+
+  const { name, email, password } = formData;
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, { displayName: name });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onChange = (e) => {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [e.target.id]: e.target.value,
+      };
+    });
+  };
+
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Welcome Back!</p>
+      </header>
+
+      <form onSubmit={onSubmit}>
+        <input
+          type="name"
+          className="nameInput"
+          placeholder="name"
+          id="name"
+          value={name}
+          onChange={onChange}
+        />
+        <input
+          type="email"
+          className="emailInput"
+          placeholder="email"
+          id="email"
+          value={email}
+          onChange={onChange}
+        />
+        <div className="passwordInputDiv">
+          <input
+            type={showPassword ? "text" : "password"}
+            className="passwordInput"
+            placeholder="password"
+            id="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <img
+          src={visibilityIcon}
+          alt="show password"
+          onClick={() => setShowPassword((prev) => !prev)}
+        />
+
+        <div className="signUpBar">
+          <p className="signUpText">Sign Up</p>
+          <button className="signUpButton">
+            <ArrowRightIcon fill="#fffff" width={34} height={34} />
+          </button>
+        </div>
+      </form>
+      <Link to={"/sign-in"} className="registerLink">
+        Sign In Instaed
+      </Link>
+    </div>
+  );
+};
+
+export default SignUp;
