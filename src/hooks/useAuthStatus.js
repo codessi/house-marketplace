3b9e3977908c216456caeaba94f51444f79aa43f
codessi <johansuh@gmail.com>
2022-08-01 15:51:11 -0700

    fulfilled private route

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,21 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
 
+  const isMounted = useRef(true)
+
   useEffect(() => {
     const auth = getAuth();
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
+        setCheckingStatus(false)
 
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true);
-      } 
-      setCheckingStatus(false)
-
-    });
-  }, []);
+      });
+    }
+    return () => {
+      isMounted.current = false
+    }
+ 
+  }, [isMounted]);
 
   return {loggedIn, checkingStatus}
 };
