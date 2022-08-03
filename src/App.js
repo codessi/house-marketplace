0fb0630c44ca93cb6d278254bf79054cd68c8d45
codessi <johansuh@gmail.com>
2022-08-03 08:27:17 -0700

    offer kinda working

--- src/App.js
+++ src/App.js
@@ -15,40 +15,15 @@ import Category from "./pages/Category";
 
 function App() {
 
-  //create ListingItem.jsx under component  [x]
-  // rafce  [x]
-  //import Link    DeleteIcon as component  bedicon bethtub  [x]
-  // destrured listing and id  pass as prop  [x]
-  //  jsx li.categoryListing wrap  [x]
-  // Link to '/category/${listing.type}/${id} .categoryListingLink  [x]
-  // img src listing.imageUrls[0] alt listing.name  class categoryListingImg  [x]
-  // gt catagory.jsx  [x]
-  // import ListingItem  compoenet  [x]
-  // in ul , listings.map((listing) => (insert*))  [x]
-  // insert <ListingItem listing = {listing.data} id= {listing.id} key ={listing.id}  [x]
-  // gt listingItem  [x]
-  // under img, .categoryListingDetails wrap  []
-  // p.categoryListingLocation wrap  [x]
-  // {listing.location}  [x]
-  // sibliing p.categoryListingName wrap {listing.name}  [x]
-  // sibling p.categoryListingPrice wrap ${lsiting.offer? listing.dicountedPrice: listing.regularPrice}  [x]
-  // convert to string, and replace  - find regex that insert comma for money  [x]
-  // sibleing  {listing.type === 'rent' && ' / Month'}  [x]
-  // sibling to p, .categoryListingInfoDiv wrap  [x]
-  // img src bedicon bed  [x]
-  // sibiling, p.categoryListingInfoText wrap  [x]
-  // {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}  [x]
-  // sibling p,  img src bathtubIcon  [x]
-  // sibling to img, p.categoryListingInfoText wrap  [x]
-  // copy {listing...} replace bed to bath  [x]
-  // pass prop onDelete  []
-  // above li closing tag, add {onDelete && ( )}  []
-  // <DeleteIcon className = 'removeIcon' fill= 'rgb(231,76,60)' onClick = {() => onDelete(listing.id, listing.name)}  [x]
-  // 
-
+  // gt category copy code  []
+  // gt offers paste  []
+  // change offer from category  []
+  // replace where  'offer, '== true  []
+  // delete child of .pageHeader  []
+  // replace with Offers  []
+  // go to firestore checkout on offer turn on off  to check on browser  []
+  // reaplce  there are no current offers from "no listing... "  []
   
-
-
   
   return (
     <>
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import Spinner from "../components/Spinner";
+import { toast } from "react-toastify";
+import ListingItem from "../components/ListingItem";
 
 const Offers = () => {
-  return (
-    <div>Offers</div>
-  )
-}
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default Offers
+  const params = useParams();
+  
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const listingsRef = collection(db, "listings");
+
+        const q = query(
+          listingsRef,
+          where("offer", "==", true),
+          // orderBy("timestamp", "desc"),
+          limit(10)
+        );
+        const querySnap = await getDocs(q);
+        // console.log(querySnap)
+        toast.success('fetch success')
+
+        let newListings = [];
+
+        querySnap.forEach((doc) => {
+          return newListings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListings(newListings);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fetch listings");
+        setLoading(false);
+      }
+    };
+    fetchListings();
+
+  }, []);
+  return (
+    <div className="category">
+      <header>
+        <p className="pageheader">
+   Offers
+        </p>
+      </header>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+                {listings.map((listing) => (
+                  <ListingItem listing={listing.data} id={listing.id} key={listing.id} />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No Listings for Offers</p>
+      )}
+    </div>
+  );
+};
+
+export default Offers;
